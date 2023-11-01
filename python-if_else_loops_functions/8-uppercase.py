c7ce61d32c9e5ef50c197f8f7cde8877f08e7596
Nyinawabagesera <j.nyinawaba@alustudent.com>
2023-11-01 11:45:16 -0700

    8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,4 +3,4 @@ def uppercase(str):
     for char in str:
         if 123 > ord(char) > 96:
             char = chr(ord(char) - 32)
-        print('{}'.format(char), end="")
+        print('{}'.format(char), end=" ")
