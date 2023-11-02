e26b4f2ff5a4001e8c1307c12ad02019bd61e434
Nyinawabagesera <j.nyinawaba@alustudent.com>
2023-11-02 03:05:04 -0700

    100-print_tebahpla.py

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-i = 122
-while i >= 97:
-    flag = 0
-    if i % 2 != 0:
-        i = i - 32
-        flag = 1
-    print("{:s}".format(chr(i)), end="")
-    if flag == 1:
-        i = i + 32
-    i = i - 1
+for letter in range(122, 96, -1):
+    if letter % 2 != 0:
+        letter -= 32
+    print("{}".format(chr(letter)), end="")
