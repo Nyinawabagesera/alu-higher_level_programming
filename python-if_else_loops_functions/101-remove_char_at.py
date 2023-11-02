59310acf01c3fe4ba76a80a6379fe2c4fce6c009
Nyinawabagesera <j.nyinawaba@alustudent.com>
2023-11-02 03:10:20 -0700

    101-remove_char_at.py

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-def magic_calculation(a, b, c):
-    if a < b:
-        return c
-    if b < c:
-        return a + b
-    return a * b - c
+def remove_char_at(str, n):
+    if n >= 0:
+        str = str[:n] + str[n + 1:]
+    return str
