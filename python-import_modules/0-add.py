867b35ca647b64414c1fc6aba13fa6faa3786a2d
Nyinawabagesera <j.nyinawaba@alustudent.com>
2023-11-03 04:23:22 -0700

    0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-   from add_0 import add
-   a = 1
-   b = 2
+#!/usr/bin/python3
+from add_0 import add
 
-   result = add(a, b)
-   print(f"{a} + {b} = {result})
-~                             
+a = 1
+b = 2
+
+if __name__ == '__main__':
+    print("{} + {} = {}".format(a, b, add(a, b)))
