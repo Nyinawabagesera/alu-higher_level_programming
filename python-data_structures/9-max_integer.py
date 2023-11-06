1766768d786c8590872b43be8febfaf3ca55e47d
Nyinawabagesera <j.nyinawaba@alustudent.com>
2023-11-06 04:17:24 -0800

    9-max_integer.py

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if not my_list:
-     return None
+        return None
     max_val = my_list[0]
     for item in my_list:
         if item > max_val:
