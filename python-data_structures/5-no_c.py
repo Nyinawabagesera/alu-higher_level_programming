54de777aca193904620ac135fa2dc5aa4d910b29
Nyinawabagesera <j.nyinawaba@alustudent.com>
2023-11-06 01:56:49 -0800

    5-no_c.py

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    return = ""
-for char in my_string:
-    if char != 'c' or char != 'C'
-        result= result + char
-        return result
+    result = ""
+    for char in my_string:
+        if char not in 'cC':
+            result += char
+    return result
