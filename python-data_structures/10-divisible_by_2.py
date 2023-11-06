9e3d8cd5f30bc66dfee7f80615c8d1b93296c192
Nyinawabagesera <j.nyinawaba@alustudent.com>
2023-11-06 04:29:41 -0800

    10-divisible_by_2.py

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,6 +2,6 @@
 def divisible_by_2(my_list=[]):
     for item in divisible_by_2(my_list):
         if item % 2 == 0:
-            return True
+            print("{:d} is divisible by 2".format(item))
         else:
-            return False
+            print("{:d} is not divisible by 2".format(item))
