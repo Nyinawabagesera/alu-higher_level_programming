2b10287d165f9aa48db903f96d6d62bf7534645a
Nyinawabagesera <j.nyinawaba@alustudent.com>
2023-11-09 00:46:08 -0800

    1-search_replace.py

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-new_list = []    
+    new_list = []    
     for element in my_list:
         if my_list == search:
             new_list = my_list.append(replace)
