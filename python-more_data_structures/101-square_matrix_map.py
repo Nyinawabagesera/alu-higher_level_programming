0236548b36768e924e162f31049ca00f1892b0ee
Nyinawabagesera <j.nyinawaba@alustudent.com>
2023-11-09 04:55:24 -0800

    101-square_matrix_map.py

--- python-more_data_structures/101-square_matrix_map.py
+++ python-more_data_structures/101-square_matrix_map.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-weight_average = __import__('100-weight_average').weight_average
-
-my_list = [(1, 2), (2, 1), (3, 10), (4, 2)]
-# = ((1 * 2) + (2 * 1) + (3 * 10) + (4 * 2)) / (2 + 1 + 10 + 2)
-result = weight_average(my_list)
-print("Average: {:0.2f}".format(result))
+def square_matrix_map(matrix=[]):
+    return list(map(lambda x: list(map(lambda y: y * y, x)), matrix))
