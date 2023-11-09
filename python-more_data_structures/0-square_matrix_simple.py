9e9e8cdeecfe644a3b9e01eecfb9e6a294993c6c
Nyinawabagesera <j.nyinawaba@alustudent.com>
2023-11-08 23:56:39 -0800

    0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    transpose = []
-    for num in range(4):
-        square_num = num**2
-        transpose.append([row[i] for row in matrix])
+     [[num*num  row[num]] for row in matrix]
 
