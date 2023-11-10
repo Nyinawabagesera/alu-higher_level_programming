54ba577d7c13ee2e9b6ee0bbc7cd7e8735797f53
Nyinawabagesera <j.nyinawaba@alustudent.com>
2023-11-10 03:04:06 -0800

    0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,7 +2,8 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
-        if count == x:
+        while True:
+            if count == x:
                 break
             print(my_list[count], end='')
             count += 1
@@ -11,5 +12,3 @@ def safe_print_list(my_list=[], x=0):
     finally:
         print()
     return count
-~                                                                                                                                                    
-~                    
