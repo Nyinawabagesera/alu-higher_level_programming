d3bd63fa91451bb883e5a2ad47b6a6eacc6b00fd
Nyinawabagesera <j.nyinawaba@alustudent.com>
2023-11-10 02:43:33 -0800

    4-list_division.py

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -3,23 +3,16 @@ def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(list_length):
         try:
-            # Attempt to perform the division
             result = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
-            # Handle division by zero
             print("division by 0")
             result = 0
-        except (TypeError, ValueError):
-            # Handle wrong type
+        except TypeError:
             print("wrong type")
             result = 0
         except IndexError:
-            # Handle index out of range
             print("out of range")
             result = 0
         finally:
-            # Append the result to the new_list
             new_list.append(result)
-            # Print a message that will always be executed
-            print("This will always be executed")
-    return new_list
+    return (new_list)
