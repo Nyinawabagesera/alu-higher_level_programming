9f98322b9d67f66b11636c6418e4b9627e0bd3bf
Nyinawabagesera <j.nyinawaba@alustudent.com>
2023-11-19 11:06:37 -0800

    11-square.py

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -10,16 +10,15 @@ class Square(Rectangle):
     """Inherits from Rectangle."""
 
     def __init__(self, size):
-        """Initializes data."""
-
+        """set the values"""
         self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
-
-    def __str__(self):
-        """Returns [Square] <width>/<height>."""
-        return str("[Square] {}/{}".format(self.__size, self.__size))
 
     def area(self):
         """Area of Square"""
-
         return self.__size ** 2
+
+    def __str__(self):
+        '''Returns [Square] '''
+        return str("[Square] {}/{}".format(self.__size, self.__size))
