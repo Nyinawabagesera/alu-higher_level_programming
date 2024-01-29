51c506d70c5c40df1fefae807f6344a4ea59ac16
Nyinawabagesera <j.nyinawaba@alustudent.com>
2024-01-29 19:40:30 +0000

    file

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,3 +1,7 @@
 #!/usr/bin/node
-const args = process.argv;
-console.log(`${args[2]} is ${args[3]}`);
+const arg = process.argv[2];
+if (arg === undefined) {
+  console.log('No argument');
+} else {
+  console.log(arg);
+}
