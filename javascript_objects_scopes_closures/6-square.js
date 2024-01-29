029b8380f7d54f2eab24e5211ff687452637465b
Nyinawabagesera <j.nyinawaba@alustudent.com>
2024-01-29 20:15:15 +0000

    file

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,17 +1,10 @@
 #!/usr/bin/node
-const Squared = require('./5-square');
-
-module.exports = class Square extends Squared {
+module.exports = class Square extends require('./5-square.js') {
   charPrint (c) {
     if (c === undefined) {
-      c = 'X';
-    }
-    for (let i = 0; i < this.height; i++) {
-      let s = '';
-      for (let j = 0; j < this.width; j++) {
-        s += c;
-      }
-      console.log(s);
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
     }
   }
 };
